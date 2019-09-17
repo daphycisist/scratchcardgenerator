78232239a0625ed0bdd8762c02026072179b4c35
daphycisist <daphycisist@gmail.com>
2019-09-17 17:29:47 +0100

    Changes made to Homepage

---
+++ Pin_Validator.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+    $('.PinSubmit').click(function(event) {
+        event.preventDefault();
+        const pinInput = $('#pinInput').val();
+        //Check if user input is empty
+        if (!pinInput) {
+          $('.inputMessage').html('Kindly input your scratch card pin');
+            $('pinInput').click(function(){
+                alert("Input pin")
+            })
+          return;
+        }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     //Make get request to check if the user already exist
+//     $.ajax({
+//       method: 'GET',
+//       url: `http://localhost:3000/users?email=${email}`,
+//       data: {
+//         email,
+//       },
+//       beforeSend: function() {
+//         $('.regMessage').html('Loading....');
+//       },
+//       success: function(response) {
+//         if (response.length) {
+//           $('.regMessage').html('User already exist');
+//         } else {
+//           //Submit the user data if the user does not exist
+//           $.ajax({
+//             method: 'POST',
+//             url: 'http://localhost:3000/users',
+//             data: {
+//               fullname,
+//               username,
+//               email,
+//               password,
+//             },
+//             beforeSend: function() {
+//               $('.regMessage').html('Loading....');
+//             },
+//             success: function() {
+//               $('.regMessage').html('Registration Successfull');
+//             },
+//           });
+//         }
+//       },
+//     });
+  });
