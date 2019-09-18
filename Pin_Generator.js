68e98f7f8bbc572dff05d9e1bcf2b75a6dd1349b
daphycisist <daphycisist@gmail.com>
2019-09-18 16:59:11 +0100

    push generated values to database

--- Pin_Generator.js
+++ Pin_Generator.js
@@ -1,24 +1,24 @@
 
 function generatePin(){
 let pin = Math.floor(Math.random()*10e15)
-let date = new Date();
-let expires = date.setTime(date.getTime()+(90*24*60*60*1000) )
-expiry ="";
-expiry=""+date.toGMTString();
-
-return {
-    "pin":[]
-
-}
+return pin;
 }
 
-
-// let data {
-//     "pin": pin,
-//     "date": new Date(),
-//     "expire": 3
-// };
-
-// $.post("http://localhost:3000/Cards", data, function() {
-
-// })
+$(document).ready(function() {
+$('.generatePin').click(function(event) {
+    event.preventDefault();
+    let generatedPin = generatePin();
+    let date = new Date();
+    let expires = date.setTime(date.getTime()+(90*24*60*60*1000) )
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/pin",
+        data: {
+            pinNumber: generatedPin,
+            expiryDate: expires
+        }
+    }).done(function() {
+        alert('Done');
+    });
+});
+});
